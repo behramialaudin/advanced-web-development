40805b781cfc6ffb607ed662fd20d25d47513f47
behramialaudin <72756138+behramialaudin@users.noreply.github.com>
2021-04-06 02:30:38 +0200

    re designed index page added bootstrap to look a bit better

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,24 +3,48 @@
 
     <link rel="stylesheet" href="/resources/css/index.css">
     <title>Index</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+
+        #memberForm{
+            margin: 0 auto;
+            width: 50%;
+        }
+
+        #btnInfo{
+            margin: 30px 0 0 30px;
+        }
+
+    </style>
 
 </head>
 
 <body>
-<button type="button" onclick="window.location='{{ route('show.members') }}'">Show Members</button><br><br>
-        <form method="post" action="{{route('create.gym.member')}}" enctype="multipart/form-data">
+<button id="btnInfo"onclick="window.location='{{ route('show.members') }}'" type="button" class="btn btn-info">Show Member</button><br><br>
+        <form id="memberForm" method="post" action="{{route('create.gym.member')}}" enctype="multipart/form-data">
             @csrf
-            <label>First Name:</label>
-            <input name="first_name" type="text"><br>
-            <label>Last Name:</label>
-            <input name="last_name" type="text"><br>
-            <label>Birthdate:</label>
-            <input name="birthdate" type="date"><br>
-            <label>Expire Date:</label>
-            <input name="expire_date" type="date"><br>
-            <label>Profile Picture:</label>
-            <input type="file" name="profile_picture" /><br><br>
-            <input type="submit"/>
+            <div class="form-group">
+                <label>First Name</label>
+                <input type="text" class="form-control" name="first_name">
+            </div>
+            <div class="form-group">
+                <label>Last Name</label>
+                <input type="text" class="form-control" name="last_name" >
+            </div>
+            <div class="form-group">
+                <label>Birthdate</label>
+                <input type="date" class="form-control" name="birthdate" >
+            </div>
+            <div class="form-group">
+                <label>Expire Date</label>
+                <input type="date" class="form-control" name="expire_date" >
+            </div>
+            <div class="form-group">
+                <label>Profile Picture</label>
+                <input type="file" class="form-control" name="profile_picture" >
+            </div>
+
+            <button type="submit" class="btn btn-success">Submit</button>
         </form>
 
 </body>
