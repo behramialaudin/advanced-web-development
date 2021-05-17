6859048a5974ed646123f4fd9c57919a8a4270da
behramialaudin <72756138+behramialaudin@users.noreply.github.com>
2021-05-17 03:13:28 +0200

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,17 +66,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $user = User::create([
+        return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
-
-
-        Mail::to($data['email'])->send(new WelcomeMail($user));
-
-        return $user;
     }
+
 
 
 
